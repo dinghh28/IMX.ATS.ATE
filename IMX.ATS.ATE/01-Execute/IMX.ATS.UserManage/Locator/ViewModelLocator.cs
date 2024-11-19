cb7001e1903e3a595e70e0084ad2c5ec60dec6b5
dhh <384387626@qq.com>
2024-11-19 17:18:37 +0800

    1.新增用户管理项目

---
+++ IMX.ATS.ATE/01-Execute/IMX.ATS.UserManage/Locator/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using H.WPF.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMX.ATS.UserManage
+{
+    public class ViewModelLocator: BaseViewModelLocator
+    {
+        public ViewModelLocator() 
+        {
+            //用户管理
+            SimpleIoc.Default.Register<UserMainViewModel>();
+            ContentControlManager.Regiter<UserMainView>();
+
+
+            //新增用户
+            SimpleIoc.Default.Register<NewUserViewModel>();
+            ContentControlManager.Regiter<NewUserView>();
+        }
+
+        public UserMainViewModel User => ServiceLocator.Current.GetInstance<UserMainViewModel>();
+
+        public NewUserViewModel NewUser => ServiceLocator.Current.GetInstance<NewUserViewModel>();
+    }
+}
